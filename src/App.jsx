993df8708e1395f89424dc966c9d32a42cce8582
rad <me@radnotred.dev>
2023-11-15 23:11:17 -0500

    update wtf

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,90 @@
-import { faGithub, faDiscord, faSpotify, faPaypal } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import { inject } from '@vercel/analytics';
-
+import {faDiscord, faGithub, faSpotify} from "@fortawesome/free-brands-svg-icons";
+import {faCommentDots, faEnvelope} from "@fortawesome/free-solid-svg-icons";
+import {inject} from '@vercel/analytics';
 import Button from "./components/Button";
 import Styles from "./App.module.scss";
-
 import mouseScrollImg from "./assets/mouse-scroll.png"
-import { onMount } from "solid-js";
+import {createSignal, onCleanup, onMount} from "solid-js";
+
+// idk why this wouldn't work when it was in App.module.scss
+// I'm also too lazy to make this look better :(
+const Notification = (prop) => {
+    return (
+        <div style={{
+            position: 'fixed',
+            top: '10px',
+            left: '50%',
+            transform: 'translateX(-50%)',
+            backgroundColor: '#4caf50',
+            color: 'White',
+            padding: '10px 20px',
+            borderRadius: '4px',
+            boxShadow: '0 2px 4px rgba(0, 0, 0, 0.2)',
+            zIndex: 1000
+        }}>
+            {`${prop.label} handle copied to clipboard`}
+        </div>
+    );
+};
 
 const App = () => {
+    const [showCopyNotification, setShowCopyNotification] = createSignal({show: false, label: ''});
+    const [timeoutId, setTimeoutId] = createSignal(null);
 
-  onMount(() => inject());
+    onMount(() => inject());
 
-  return (
-    <>
-      <div className={Styles.Modal}>
-        <div className={Styles.ContentLeft}>
-          <img src="https://avatars.githubusercontent.com/u/80015713?v=4" />
-          <h1>rad</h1>
-          <p>Unskilled programmer</p>
-        </div>
-        <div className={Styles.ContentRight}>
-          <ul>
-            <Button href="https://github.com/RadNotRed" icon={faGithub} label="GitHub" />
-            <Button href="https://discord.com/users/610945092504780823" icon={faDiscord} label="Discord" />
-            <Button href="https://open.spotify.com/user/31ktcyhbhbwssktqfgpdb5cznydy" icon={faSpotify} label="Spotify" />
-            <Button href="https://paypal.me/radnotred" icon={faPaypal} label="PayPal" />
-            <Button href="mailto:me@radnotred.dev" icon={faEnvelope} label="E-Mail" />
-          </ul>
-        </div>
-        <a href="#about" className={Styles.ScrollIndicator}>
-          <img src={mouseScrollImg} />
-        </a>
-        <div id="about" className={Styles.ContentBottom}>
-          <p>
-            Hi, I'm rad,
-            I'm going to college for Criminal Justice Technology, but I'm also a programmer when I feel like it. 
-            I'm currently working on an app for my high school, which is written in <span>Dart</span> and <span>Flutter</span> as the framework.
-            I currently use <span>TypeScript</span>, <span>Python</span>, and <span>Java</span>.
+    const handleCopy = (text, source) => {
+        navigator.clipboard.writeText(text);
+        if (timeoutId()) clearTimeout(timeoutId()); // clear previous timeout when new one is created
+        setShowCopyNotification({show: true, label: source});
+        setTimeoutId(setTimeout(() => setShowCopyNotification({show: false, label: ''}), 3000));
+    };
 
-            My only discord accounts are @radnotred1 and @radacul. No others are mine. If you cannot add me on there, you can add my matrix account @radnotred:matrix.org.
-          </p>
-        </div>
-      </div>
-    </>
-  );
+    onCleanup(() => {
+        if (timeoutId()) clearTimeout(timeoutId()); // clear timeout when component is unmounted
+    });
+
+    return (
+        <>
+            {showCopyNotification().show && <Notification label={showCopyNotification().label}/>}
+            <div className={Styles.Modal}>
+                <div className={Styles.ContentLeft}>
+                    <img src="https://avatars.githubusercontent.com/u/80015713?v=4"/>
+                    <h1>rad</h1>
+                    <p>Not a federal agent</p>
+                </div>
+                <div className={Styles.ContentRight}>
+                    <ul>
+                        <Button href="https://github.com/RadNotRed" icon={faGithub} label="GitHub"/>
+                        <Button
+                            href="https://discord.com/"
+                            onClick={() => handleCopy('@radnotred1', 'Discord')}
+                            icon={faDiscord}
+                            label="Discord"
+                        />
+                        <Button
+                            href="https://matrix.org/"
+                            onClick={() => handleCopy('@radnotred:matrix.org', 'Matrix')}
+                            icon={faCommentDots}
+                            label="Matrix Chat"
+                        />
+                        <Button href="https://open.spotify.com/user/31ktcyhbhbwssktqfgpdb5cznydy" icon={faSpotify} label="Spotify"/>
+                        <Button href="mailto:me@radnotred.dev" icon={faEnvelope} label="E-Mail"/>
+                    </ul>
+                </div>
+                <a href="#about" className={Styles.ScrollIndicator}>
+                    <img src={mouseScrollImg}/>
+                </a>
+                <div id="about" className={Styles.ContentBottom}>
+                    <p style={{ lineHeight: "1.6" }}>
+                        I'm rad, I'm going to college for Law Enforcement Technology. I use <span>TypeScript</span> and nothing else right now.<br/>
+                        <br/>
+                        You can add me on matrix if you want to talk to me.<br/>
+                    </p>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,16 +1,26 @@
 import Fa from "solid-fa";
-
 import Styles from "./Button.module.scss";
 
+const isModifiedEvent = (event) =>
+    !!(event.metaKey || event.altKey || event.ctrlKey || event.shiftKey);
+
 const Button = (props) => {
+    const handleClick = (e) => {
+        if (!props.onClick) return;
+
+        if (isModifiedEvent(e)) return;
+
+        e.preventDefault();
+        props.onClick();
+    };
+
     return (
         <li class={Styles.Button}>
-            <a href={props.href}>
+            <a href={props.href} onClick={handleClick}>
                 <Fa icon={props.icon} />
                 <span>{props.label}</span>
             </a>
         </li>
     );
 };
-
 export default Button;
